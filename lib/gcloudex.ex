6577172c7fc70ed08742f16f9bb2c74e34c2ce24
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2019-06-06 17:30:43 -0300

    Fixing Jason.decode!/1 call

--- lib/gcloudex.ex
+++ lib/gcloudex.ex
@@ -11,7 +11,7 @@ defmodule GCloudex do
   def get_project_id do
     :goth
     |> Application.get_env(:json)
-    |> Jason.decode!(%{})
+    |> Jason.decode!()
     |> Map.get("project_id")
   end
 end
